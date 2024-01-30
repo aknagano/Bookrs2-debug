54a09398a8a7840330fab5a5285cc1bdfa8f7758
aknagano <150144545+aknagano@users.noreply.github.com>
2024-01-30 13:05:17 +0000

    Initial Commit

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  
+  get "home/about"=>"homes#about"
+  get "home/top"=>"homes#top"
+  devise_for :users
+  
+  resources :books, only: [:index,:show,:edit,:create,:destroy,:update]
+  resources :users, only: [:index,:show,:edit,:update]
+  
+  root to: "homes#top"
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
