f0402441d5fd55ac6fec2b6728b255d337c11504
aknagano <150144545+aknagano@users.noreply.github.com>
2024-02-02 09:22:51 +0000

    Follower

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -4,7 +4,12 @@ class PostCommentsController < ApplicationController
     comment = current_user.post_comments.new(post_comment_params)
     comment.book_id = book.id
     comment.save
-    redirect_to book_path(book)
+    redirect_to request.referer
+  end
+  
+  def destroy
+    PostComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
+    redirect_to request.referer
   end
 
   private
